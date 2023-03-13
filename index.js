70be3b24b457f11da15867bd6052c1c390ae6a9a
faridsunkar <farid.sunkar@student.moringaschool.com>
2023-03-13 14:38:50 -0700

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"] ;
+
+function destructivelyAppendCat(name="Ralph"){
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name="Bob"){
+    cats.unshift(name)
+
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop(name)
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name)
+}
+
+function appendCat(name="Broom"){
+    const cats2 = cats.slice();
+    cats2.push(name)
+    return cats2
+}
+
+function prependCat(name="Arnold"){
+    const cats3 =cats.slice();
+    cats3.unshift(name)
+    return cats3
+
+}
+
+function removeLastCat(){
+    const cats4 =cats.slice();
+    cats4.pop()
+    return cats4
+}
+
+function  removeFirstCat(){
+    const cats5 =cats.slice();
+    cats5.shift()
+    return cats5
+}
+
